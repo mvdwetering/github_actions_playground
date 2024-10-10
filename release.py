e7387238f2dcdbfdda536d15e4c9e74333dc792f
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2024-10-10 19:56:24 +0200

    Fail on subprocess errors and fix print

--- release.py
+++ release.py
@@ -57,40 +57,39 @@ class Git:
 
     @staticmethod
     def checkout(branch):
-        subprocess.run(["git", "checkout", branch])
+        subprocess.run(["git", "checkout", branch], check=True)
 
     @staticmethod
     def add_changes():
-        subprocess.run(["git", "add", "--all"])
+        subprocess.run(["git", "add", "--all"], check=True)
 
     @staticmethod
     def commit_changes(message):
-        subprocess.run(["git", "commit", "-m", message])
+        subprocess.run(["git", "commit", "-m", message], check=True)
 
     @staticmethod
     def pull():
-        subprocess.run(["git", "pull"])
+        subprocess.run(["git", "pull"], check=True)
 
     @staticmethod
     def delete_branch(name):
-        subprocess.run(["git", "branch", "-D", name])
+        subprocess.run(["git", "branch", "-D", name], check=True)
 
     @staticmethod
     def create_branch(name):
-        subprocess.run(["git", "branch", name])
+        subprocess.run(["git", "branch", name], check=True)
 
     @staticmethod
     def create_tag(name):
-        subprocess.run(["git", "tag", name])
+        subprocess.run(["git", "tag", name], check=True)
 
     @staticmethod
     def push_to_origin(name):
-        subprocess.run(["git", "push", "origin", name])
+        subprocess.run(["git", "push", "origin", name], check=True)
 
     @staticmethod
     def fetch_tags():
-        subprocess.run(["git", "fetch", "--tags"])
-
+        subprocess.run(["git", "fetch", "--tags"], check=True)
 
 def menu(title, choices):
     while True:
@@ -352,8 +351,8 @@ def main(args):
             Git.push_to_origin(MASTER)
         Git.push_to_origin(release_branch_name)
         Git.push_to_origin(tag_name)
+    else:
         print("Don't forget to push later or revert changes!")
-
 
     print("Done!")
     print(f"Currently on branch: {Git.get_current_branch().name}")
